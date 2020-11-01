41f2492b57ceeec9f8d2ca76494ecc67e8bef072
Alexandr <k.v.alexandr@gmail.com>
2020-11-01 21:38:39 +0400

    feat: add adaptive

--- virtual-keyboard/script.js
+++ virtual-keyboard/script.js
@@ -29,8 +29,8 @@ const Keyboard = {
     ['Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
     ['KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
     ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
-    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'Voice', 'Volume'],
-    ['Done', 'Lang', 'Space', 'ArrowLeft', 'ArrowRight']
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash'],
+    ['Voice', 'Volume', 'Lang', 'Space', 'ArrowLeft', 'ArrowRight', 'Done']
   ],
 
   init() {
